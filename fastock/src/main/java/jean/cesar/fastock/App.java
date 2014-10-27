d81fa2eb4645764427528ff56764dcb9ac7c0545
jean pierre cid <jeanpierre.cid@gmail.com>
2014-10-27 11:49:54 -0300

    solo prueba

--- fastock/src/main/java/jean/cesar/fastock/App.java
+++ fastock/src/main/java/jean/cesar/fastock/App.java
@@ -14,5 +14,6 @@ public class App
         Loginn loggin = new Loginn();
         loggin.setVisible(true);
         loggin.setLocationRelativeTo(null);
+        JOptionPane.showMessageDialog(loggin, args);
     }
 }
